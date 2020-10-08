c1fba5e1196955968b444c392467359068957148
Julien Devergnies <j.devergnies@outlook.com>
2020-10-09 00:35:42 +0200

    fixed : improvements responsive

--- components/header.vue
+++ components/header.vue
@@ -3,9 +3,8 @@
         <nav class="navbar">
             <div class="left">
                 <nuxt-link to="/"><span class="app-name">{{ appName }}</span></nuxt-link>
-                <!-- <nuxt-link to="/">accueil</nuxt-link> -->
-                <nuxt-link to="/blog">blog</nuxt-link>
-                <nuxt-link to="/apropos">à propos</nuxt-link>
+                <nuxt-link to="/blog" class="sm-hide">blog</nuxt-link>
+                <nuxt-link to="/apropos" class="sm-hide">à propos</nuxt-link>
             </div>
             <div class="right">
                 <a :href="github" target="_blank" class="icon">
@@ -41,14 +40,12 @@ export default {
 
 <style scope>
 .app-name {
-    color: #333333;
     font-size: 18px;
     font-weight: bold;
 }
 
 
 header.header {
-    background-color: #fdfdff;
     box-shadow: 0 0 6px rgba(#000000, 0.3);
     left: 0;
     position: fixed;
