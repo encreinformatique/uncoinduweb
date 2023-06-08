334251df9634538018c8bffeaff2a9f9b4477bd5
Julien Devergnies <j.devergnies@outlook.com>
2023-06-08 23:45:31 +0200

    fixed : contenu pour article github ssh deploy key

--- components/content/AppImg.vue
+++ components/content/AppImg.vue
@@ -1,14 +1,26 @@
 <template>
-    <div>
+    <picture class="image">
         <img :src="source" :alt="alt">
-    </div>
+    </picture>
 </template>
 
-<script setup>
-const props = defineProps({
-    filename: { type: String, required: true },
-    alt: { type: String, required: false }
-});
+<script>
+export default {
+    name: 'AppImg',
+    props: {
+        filename: { type: String, required: true },
+        alt: { type: String, required: false }
+    },
+    computed: {
+        source() {
+            return import(/* @vite-ignore */`/${this.filename}`);
+        }
+    }
+}
+// const props = defineProps({
+//     filename: { type: String, required: true },
+//     alt: { type: String, required: false }
+// });
 
-const source = computed(() => import(/* @vite-ignore */ `~/assets/${this.filename}`));
+// const source = computed(() => import(/* @vite-ignore */ `~/assets/${this.filename}`));
 </script>
